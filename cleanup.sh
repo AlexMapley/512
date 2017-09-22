559e1b172bd027e0bc5719e31b916ffd522ccd39
Alexander MAPLEY <amaple1@ds2.CS.McGill.CA>
2017-09-22 14:53:06 -0400

    Adds client setup

--- cleanup.sh
+++ cleanup.sh
@@ -1,6 +1,6 @@
 rm servercode/ResImpl/*.class
-rm *.swp
+rm .*swp
 rm servercode/*.jar
-kill -9 $(lsof +D `pwd` | grep ".nfs" | awk '{print $2}')
+kill -9 $(ps -aux | grep ".nfs" | awk '{print $2}')
 kill -9 $(ps -aux | grep "rmiregistry -J" | awk '{print $2}')
 
