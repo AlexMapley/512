e8628651ceb218bf3c5c5a822d3491c51d96209c
Alexander MAPLEY <amaple1@ds2.CS.McGill.CA>
2017-09-22 13:04:40 -0400

    implements server setup

--- create_server.sh
+++ create_server.sh
@@ -1,9 +1,20 @@
+# Register RMI
+rmiregistry -J-Djava.rmi.server.useCodebaseOnly=false 1099 &
+
 # Exporting Class Path
 ClassPath="`pwd`""/servercode"
 echo $ClassPath
 export CLASSPATH="`pwd`""/servercode"
 
-#Compiling codebase
+# Compiling codebase
 javac servercode/ResInterface/ResourceManager.java
 jar cvf servercode/ResInterface.jar servercode/ResInterface/*.class
 javac servercode/ResImpl/ResourceManagerImpl.java
+
+# Setting permissions
+chmod 704 servercode/ResInterface.jar
+chmod 704 servercode/ResInterface/*.class
+chmod 705 servercode # directory needs to be executable???
+
+# Run server on registered instance
+java -Djava.security.policy=java.policy -Djava.rmi.server.codebase="`pwd`""/servercode" ResImpl.ResourceManagerImpl 1099
