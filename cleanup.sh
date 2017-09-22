312a9f613501b13d5da473de634926f208e3e498
Alexander MAPLEY <amaple1@ds2.CS.McGill.CA>
2017-09-22 14:04:34 -0400

    unlocking server

--- cleanup.sh
+++ cleanup.sh
@@ -2,3 +2,5 @@ rm servercode/ResImpl/*.class
 rm *.swp
 rm servercode/*.jar
 kill -9 $(lsof +D `pwd` | grep ".nfs" | awk '{print $2}')
+kill -9 $(ps -aux | grep "rmiregistry -J" | awk '{print $2}')
+
