f2308615c1580ddd63ccd8204b51862bc6e787b3
Alexander MAPLEY <amaple1@ds2.CS.McGill.CA>
2017-09-30 21:12:59 -0400

    more stuff

--- tcp/servercode/ResImpl/MiddleWareServerThread.java
+++ tcp/servercode/ResImpl/MiddleWareServerThread.java
@@ -21,6 +21,7 @@ public class MiddleWareServerThread extends Thread {
   	try {
       // Declaring Socket Variables:
       String message = null;
+      String res = "@ no RM response @";
 
       //Client
       BufferedReader inFromClient = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
@@ -36,46 +37,60 @@ public class MiddleWareServerThread extends Thread {
       BufferedReader inFromCarRM = new BufferedReader(new InputStreamReader(carSocket.getInputStream()));
   		PrintWriter outToCarRM = new PrintWriter(carSocket.getOutputStream(), true);
 
-      //Rooms
+      // //Rooms
       Socket roomSocket = new Socket("lab1-5", 5959);
       BufferedReader inFromRoomRM = new BufferedReader(new InputStreamReader(roomSocket.getInputStream()));
   		PrintWriter outToRoomRM = new PrintWriter(roomSocket.getOutputStream(), true);
 
       while ((message = inFromClient.readLine())  != null) {
-    		System.out.println("message from client:" + message);
+    		System.out.println("\nmessage from client: " + message);
 
-        // Parsing command for RM
+        // Parsing commands for RM:
+
+        // Flights
     		String[] params =  message.split(",");
         if (params[0].compareToIgnoreCase("newflight")==0 || params[0].compareToIgnoreCase("deleteflight")==0
             || params[0].compareToIgnoreCase("queryflight")==0  || params[0].compareToIgnoreCase("queryflightprice")==0
             || params[0].compareToIgnoreCase("reserveflight")==0 )
             {
-              System.out.println("Out to Flight RM!");
+              System.out.println("Out to Flight RM");
+              outToFlightRM.println(message);
+              res = inFromFlightRM.readLine();
         }
+        // Cars
         else if (params[0].compareToIgnoreCase("newcar")==0 || params[0].compareToIgnoreCase("deletecar")==0
             || params[0].compareToIgnoreCase("querycar")==0  || params[0].compareToIgnoreCase("querycarprice")==0
             || params[0].compareToIgnoreCase("reservecar")==0 )
             {
-              System.out.println("Out to Car RM!");
+              System.out.println("Out to Car RM");
+              outToCarRM.println(message);
+              res = inFromCarRM.readLine();
         }
+        // Rooms
         else if (params[0].compareToIgnoreCase("newroom")==0 || params[0].compareToIgnoreCase("deleteroom")==0
             || params[0].compareToIgnoreCase("queryroom")==0  || params[0].compareToIgnoreCase("queryroomprice")==0
             || params[0].compareToIgnoreCase("reserveroom")==0 )
             {
-              System.out.println("Out to Room RM!");
+              System.out.println("Out to Room RM");
+              outToRoomRM.println(message);
+              res = inFromRoomRM.readLine();
         }
+        // Other
         else if (params[0].compareToIgnoreCase("newcustomer")==0 || params[0].compareToIgnoreCase("deletecustomer")==0
             || params[0].compareToIgnoreCase("itinerary")==0  || params[0].compareToIgnoreCase("newcustomerid")==0
             || params[0].compareToIgnoreCase("quit")==0 )
             {
-              System.out.println("Out to NO RM!");
+              System.out.println("Out to No RM / General Command");
+        }
+        else {
+          System.out.println("Invalid Command, not sent");
         }
 
-        String res = "LOLCAKES";
     		outToClient.println("hello client from server THREAD, your result is: " + res );
   		}
 
     clientSocket.close();
+    flightSocket.close();
   	}
   	catch (IOException e) {
   	}
