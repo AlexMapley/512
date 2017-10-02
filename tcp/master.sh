b2dae56d34d57086cb85135b1847a2058c931818
AlexMapley <alexmapley@gmail.com>
2017-10-02 19:43:13 -0400

    setup

--- tcp/master.sh
+++ tcp/master.sh
@@ -1,15 +1,19 @@
-# Create RM server ons lab1-4,5,6
-ssh `whoami`@lab1-4.cs.mcgill.ca "bash -s" < ./cleanup.sh
-ssh `whoami`@lab1-4.cs.mcgill.ca "bash -s" < ./servercode/create_server.sh
-ssh `whoami`@lab1-5.cs.mcgill.ca "bash -s" < ./cleanup.sh
-ssh `whoami`@lab1-5.cs.mcgill.ca "bash -s" < ./servercode/create_server.sh
-ssh `whoami`@lab1-6.cs.mcgill.ca "bash -s" < ./cleanup.sh
-ssh `whoami`@lab1-6.cs.mcgill.ca "bash -s" < ./servercode/create_server.sh
-
-# Create Middleware server on lab1-2
-ssh `whoami`@lab1-7.cs.mcgill.ca "bash -s" < ./cleanup.sh
-ssh `whoami`@lab1-7.cs.mcgill.ca "bash -s" < ./servercode/create_middleware.sh
-
-# Create Client Server on local instance
-ssh `whoami`@lab1-1.cs.mcgill.ca "bash -s" < ./cleanup.sh
-ssh `whoami`@lab1-1.cs.mcgill.ca "bash -s" < ./servercode/create_middleware.sh
+printf "
+\n\n\n\n##################################################################
+\n# Create RM server ons lab1-4,5,6
+\nssh `whoami`@lab1-4.cs.mcgill.ca bash -s < ./cleanup.sh
+\nssh `whoami`@lab1-4.cs.mcgill.ca bash -s < ./servercode/create_server.sh
+\nssh `whoami`@lab1-5.cs.mcgill.ca bash -s < ./cleanup.sh
+\nssh `whoami`@lab1-5.cs.mcgill.ca bash -s < ./servercode/create_server.sh
+\nssh `whoami`@lab1-6.cs.mcgill.ca bash -s < ./cleanup.sh
+\nssh `whoami`@lab1-6.cs.mcgill.ca bash -s < ./servercode/create_server.sh
+\n
+\n# Create Middleware server on lab1-7
+\nssh `whoami`@lab1-7.cs.mcgill.ca bash -s < ./cleanup.sh
+\nssh `whoami`@lab1-7.cs.mcgill.ca bash -s < ./servercode/create_middleware.sh
+\n
+\n# Create Client Server on local instance
+\nssh `whoami`@lab1-1.cs.mcgill.ca bash -s < ./cleanup.sh
+\nssh `whoami`@lab1-1.cs.mcgill.ca bash -s < ./servercode/create_middleware.sh
+\n\n\n\n##################################################################
+\n"
