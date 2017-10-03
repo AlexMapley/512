fc2922d2dcce4d4410450d1fc1bed42e99228ed7
Galen Bryant <gbryant@xometry.com>
2017-10-02 23:30:41 -0400

    please work

--- tcp/servercode/ResImpl/ResourceManagerServerThread.java
+++ tcp/servercode/ResImpl/ResourceManagerServerThread.java
@@ -76,7 +76,8 @@ public class ResourceManagerServerThread extends Thread {
     try {
       Class thisClass = Class.forName("ResImpl.ResourceManagerImpl");
       Method m = thisClass.getDeclaredMethod(convertCommand((String) args[0]), params);
-      System.out.println(m.invoke(this.host, paramsObj).toString());
+      Object object = m.invoke(this.host, paramsObj);
+      System.out.println("returned value: " + object);
       res = "RM: Succesffuly called " + args[0];
     }
     catch(NoSuchMethodException e) {
