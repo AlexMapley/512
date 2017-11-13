0b461c6f2034e2e7a65b22da5d3f7a183003ea08
AlexMapley <alexmapley@gmail.com>
2017-11-12 19:03:53 -0500

    more features

--- rmi/clientsrc/loop_client.sh
+++ rmi/clientsrc/loop_client.sh
@@ -11,35 +11,35 @@ echo "arg2 is either flight/car/room, for the target RM"
 javac client.java
 
 # RM to target
-keyword="flight"  #default
-keyword=$2
-kewyword="new"$keyword
+keyword="newflight"  #default
+keyword="new"$2
 
 # Create stdin pipe
-rm inPipe
-mkfifo inPipe
+mkfifo inPipe$$
+ls -la
 
 # Spawns new client with pipe
 # tail -f inPipe | java -Djava.security.policy=java.policy client $1 > outPipe &
-tail -f inPipe | java -Djava.security.policy=java.policy client $1 > outLog &
+tail -f inPipe$$ | java -Djava.security.policy=java.policy client $1 > outLog$$ &
 processId=$!
 
 # Feeds client-pipe commands in timed loop
 for i in `seq 1 50`;
   do
-  if [[ $kewyord == "newflight "]]; then
-    value="$keyword,$i,$i,3,4"
+  if [[ $keyword == "newflight" ]]; then
+    value="$keyword,1,$i,3,4"
   else
-    value="$keyword,mtl$i,3,4"
+    value="$keyword,1,mtl,3,4"
   fi
-  echo $value > inPipe
+  echo $value
+  echo $value > inPipe$$
   break=0
   while [[ $break -eq 0 ]]
   do
     sleep 0.5
-    if [[ `tail -1 outLog` = ">" ]]; then
+    if [[ `tail -1 outLog$$` = ">" ]]; then
       sleep 0.5
-      tail -9 outLog
+      tail -9 outLog$$
       break=1
       break
     fi
@@ -50,6 +50,8 @@ done
 # Cleanup
 # Kill client process
 kill -9 $processId
-rm outLog
-rm inPipe
+rm outLog$$
+rm inPipe$$
 kill -9 $(lsof +D `pwd` | awk '!/bash/' | awk '!/lsof/' | awk '{print $2}')
+kill -9 $(ps -aux | grep ".nfs" | awk '{print $2}')
+rm .nfs*
