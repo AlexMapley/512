35ed875ec45002f3ddd531710cb2f6f8b2614e0a
Alex Mapley <Alex@Alexs-MacBook-Pro.local>
2017-11-26 14:38:20 -0500

    Storing Hashtables

--- rmi/servercode/ResImpl/RMHashtable.java
+++ rmi/servercode/ResImpl/RMHashtable.java
@@ -5,20 +5,21 @@
 package ResImpl;
 
 import java.util.*;
+import java.io.Serializable;
 
 
 // A specialization of Hashtable with some
 // extra diagnostics
 
-public class RMHashtable extends Hashtable
-{
-  	RMHashtable() {
-		super();
+public class RMHashtable extends Hashtable implements serializable  {
+
+
+    RMHashtable() {
+		    super();
   	}
 
 
- 	public String toString()
-	{
+ 	public String toString()  {
 		String s = "--- BEGIN RMHashtable ---\n";
 		Object key = null;
 		for (Enumeration e = keys(); e.hasMoreElements(); ) {
@@ -28,11 +29,30 @@ public class RMHashtable extends Hashtable
 		}
 		s = s + "--- END RMHashtable ---";
 		return s;
-  	}
+  }
 
-  	public void dump()
-	{
+
+  public void dump() {
 		System.out.println( toString() );
-  	}
+  }
 
+
+  public void store(String filename) {
+    try {
+
+        // Initialize File/Serailization Streams
+		    FileOutputStream file_pipe = new FileOutputStream(new File(filename));
+		    ObjectOutputStream object_pipe = new ObjectOutputStream(file_pipe);
+
+		    // Serializes RM to file
+		    object_pipe.writeObject(this);
+
+        file_pipe.close();
+        object_pipe.close();
+	  }
+    catch (IOException e) {
+		     System.out.println("Error initializing stream");
+         e.printStackTrace();
+	  }
+  }
 }
